c417a2ce6c3f4b10d4580a901b1b58c4449c743d
Bryant Biggs <bryantbiggs@gmail.com>
2024-05-30 18:59:03 -0400

    chore: Remove default thats not valid for others

--- example/variables.tf
+++ example/variables.tf
@@ -1,5 +1,4 @@
 variable "role_arn" {
   description = "The ARN of the role to assume in the secondary account to provision resources ('account_b' IAM role)"
   type        = string
-  default     = "arn:aws:iam::361548816046:role/terraform"
 }
